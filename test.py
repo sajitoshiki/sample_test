0ad6f5c0726f06676674f2368155b5ac545d0533
ToshikiSaji <sajit@uniontool.co.jp>
2024-02-29 18:03:29 +0900

    add .env, test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 print("Hello!")
 
 print("This is Hello project")
 
 print("Please edit this file")
+
+key = os.environ["SOMETHING_KEY"]
+
+print(f"{key}")
